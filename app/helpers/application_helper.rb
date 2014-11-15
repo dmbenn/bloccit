3121c832334b8442d59d0696de548fc1d878d1f6
dmbenn <dmbennet@gmail.com>
2014-11-15 17:55:00 +0000

    Added post validation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,12 @@ module ApplicationHelper
     "Dan Bennett"
   end
 
+  def form_group_tag(errors, &block)
+    if errors.any?
+      content_tag :div, capture(&block), class: 'form-group has-error'
+    else
+      content_tag :div, capture(&block), class: 'form-group'
+    end
+  end
+  
 end
