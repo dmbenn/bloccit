8fc265afa0593bc39e006256906e9261977f83c4
dmbenn <dmbennet@gmail.com>
2014-11-20 01:43:39 +0000

    configured simple form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,13 +2,4 @@ module ApplicationHelper
   def my_name
     "Dan Bennett"
   end
-
-  def form_group_tag(errors, &block)
-    if errors.any?
-      content_tag :div, capture(&block), class: 'form-group has-error'
-    else
-      content_tag :div, capture(&block), class: 'form-group'
-    end
-  end
-  
 end
