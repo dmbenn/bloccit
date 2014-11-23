bef23403c2d7e6dc52eb9cdef61e84a94c53b482
dmbenn <dmbennet@gmail.com>
2014-11-23 14:19:34 +0000

    moved logic to post model

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,12 +2,5 @@ module ApplicationHelper
   def my_name
     "Dan Bennett"
   end
-
-  def markdown(text)
-    renderer = Redcarpet::Render::HTML.new
-    extensions = {fenced_code_blocks: true}
-    redcarpet = Redcarpet::Markdown.new(renderer, extensions)
-    (redcarpet.render text).html_safe
-  end
   
 end
