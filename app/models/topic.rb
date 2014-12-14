d5d78b526ed9de1b5448949b42f096cbc1113178
Dan Bennett <dmbennet@gmail.com>
2014-12-14 17:02:01 -0600

    added scope within topics

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,4 +2,9 @@ class Topic < ActiveRecord::Base
 
   has_many :posts, dependent: :destroy
 
+  scope :visible_to, -> (user) { user ? all : publicly_viewable}
+
+  scope :publicly_viewable, -> { where(public: true) }
+  scope :privatly_viewable, -> { where(public: false)}
+
 end
