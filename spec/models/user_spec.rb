64d8113f34f13d65ada24ae3a1fd4be43389ccba
Dan Bennett <dmbennet@gmail.com>
2014-12-14 17:08:59 -0600

    fixed user model test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,12 +12,12 @@ describe User do
     end
     
     it "returns 'nil' if the user has not favorited the post" do
-      expect( @user.favorites.find_by_post_id(@post.id) ).to be_nil
+      expect( @user.favorited(@post)).to be_nil
     end
 
     it "returns the appropriate favorite if it exists" do
       favorite = @user.favorites.where(post: @post).create
-      expect( @user.favorites.find_by_post_id(@post.id) ).to eq(favorite)
+      expect( @user.favorited(@post) ).to be
     end
   end 
 end
