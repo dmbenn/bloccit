26f8af8acccbd462d669eab1acdb4262e7898a8a
Dan Bennett <dmbennet@gmail.com>
2015-01-04 21:00:22 -0600

    Added user testing and factory girls, refactored specs for factorygirl

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -2,12 +2,10 @@ require 'rails_helper'
 
 describe Vote do
 
-  include TestFactories
-
   describe "validations" do
 
     before do
-      @post = associated_post
+      @post = create(:post)
     end
 
 
@@ -32,7 +30,7 @@ describe Vote do
 
     describe 'after_save' do
     it "calls `Post#update_rank` after save" do
-     post = associated_post
+     post = create(:post)
      vote = Vote.new(value: 1, post: post)
      expect(post).to receive(:update_rank)
      vote.save
